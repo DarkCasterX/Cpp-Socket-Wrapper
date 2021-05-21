3c3cd1e83f1e088f49ca872688e05d6e3df854b6
DarkCasterX <53799437+DarkCasterX@users.noreply.github.com>
2021-05-20 20:12:44 -0400

    nun

--- testClient.cpp
+++ testClient.cpp
@@ -6,10 +6,10 @@ int main(int argc, char** argv)
     {
         TCPSocket sock = TCPSocket(AF_INET, SOCK_STREAM);
         sock.Connect("127.0.0.1", 4444);
-        sock.Bind("127.0.0.1", 80);
-        sock.Send("Hello\n");
-        std::cout << std::atoi(sock.RecvChar(1024));
         sock.Close();
+        /*sock.Send("Hello\n");
+        std::cout << std::atoi(sock.RecvChar(1024));
+        sock.Close();*/
     }
     catch(SockError& err)
     {
